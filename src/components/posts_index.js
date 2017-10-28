6525b641cdaca1e8638f39c9a1765e10205159cf
Rus Hughes <russell.hughes@gmail.com>
2017-10-28 14:47:14 +0700

    section 5 lecture 127

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+
+  componentDidMount() {
+    // called as soon as the component appears in the DOM
+    // after the component renders to the screen
+    // lets do an initial import of posts
+    this.props.fetchPosts();
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +20,4 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
